111054f8f132cbfd5e05700314c60cb7c538edf5
sayakpaul <spsayakpaul@gmail.com>
2023-07-18 22:21:55 +0530

    add: support for serializing in chunks.

--- data/prepare_dataset.py
+++ data/prepare_dataset.py
@@ -7,14 +7,41 @@ from typing import Dict
 
 MIRROR_DIRECTORY = "hf_public_repos"
 DATASET_ID = "hf-codegen"
+SERIALIZE_IN_CHUNKS = 50000
 
 # Block the following formats.
 IMAGE = ["png", "jpg", "jpeg", "gif"]
 VIDEO = ["mp4", "jfif"]
-DOC = ["key", "PDF", "pdf", "docx", "xlsx"]
-AUDIO = ["flac", "ogg", "mid", "webm"]
-ARCHIVE = ["jar", "aar", "gz"]
-OTHERS = ["npy", "index", "inv", "index", "DS_Store", "rdb", "pack", "idx"]
+DOC = [
+    "key",
+    "PDF",
+    "pdf",
+    "docx",
+    "xlsx",
+    "pptx",
+]
+AUDIO = ["flac", "ogg", "mid", "webm", "wav", "mp3"]
+ARCHIVE = ["jar", "aar", "gz", "zip", "bz2"]
+MODEL = ["onnx", "pickle", "model", "neuron"]
+OTHERS = [
+    "npy",
+    "index",
+    "inv",
+    "index",
+    "DS_Store",
+    "rdb",
+    "pack",
+    "idx",
+    "glb",
+    "gltf",
+    "len",
+    "otf",
+    "unitypackage",
+    "ttf",
+    "xz",
+    "pcm",
+    "opus",
+]
 ANTI_FOMATS = tuple(IMAGE + VIDEO + DOC + AUDIO + ARCHIVE + OTHERS)
 
 
@@ -68,7 +95,7 @@ def read_repository_files(directory) -> pd.DataFrame:
         for file in files:
             file_path = os.path.join(root, file)
             if not file_path.endswith(ANTI_FOMATS) and all(
-                k not in file_path for k in [".git", "__pycache__"]
+                k not in file_path for k in [".git", "__pycache__", "xcodeproj"]
             ):
                 file_paths.append((os.path.dirname(root), file_path))
 
@@ -76,17 +103,20 @@ def read_repository_files(directory) -> pd.DataFrame:
     print(f"Total file paths: {len(file_paths)}.")
     print("Reading file contents...")
 
-    for directory_name, file_path in tqdm(file_paths):
+    for i, (directory_name, file_path) in enumerate(tqdm(file_paths)):
         file_content = process_file(directory_name, file_path)
         if file_content["content"] != "":
             temp_df = pd.DataFrame.from_dict([file_content])
             df = pd.concat([df, temp_df])
-
+            if SERIALIZE_IN_CHUNKS and ((i + 1) % SERIALIZE_IN_CHUNKS == 0):
+                df.to_csv(f"df_chunk_{i+1}_{len(df)}", index=False)
+                df = pd.DataFrame(columns=["repo_id", "file_path", "content"])
     return df
 
 
 if __name__ == "__main__":
     df = read_repository_files(MIRROR_DIRECTORY)
     print("DataFrame created, creating dataset...")
-    dataset = Dataset.from_pandas(df)
-    dataset.push_to_hub(DATASET_ID, private=True)
+    if not SERIALIZE_IN_CHUNKS:
+        dataset = Dataset.from_pandas(df)
+        dataset.push_to_hub(DATASET_ID, private=True)
